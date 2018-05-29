82448de31fd0b229222d3417b780ebcd6815950a
FuhrerTaco <36234256+FuhrerTaco@users.noreply.github.com>
2018-05-29 13:18:46 -0400

    Add files via upload

---
+++ u5GameOfLife/Cell.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace u5GameOfLife
+{
+    public class Cell
+    {
+        public static int size = 30;
+        public bool isAlive;
+        Rectangle body = new Rectangle();
+        Point location;
+        public Point Location { get => location; }
+        public Cell(bool alive, Point loc, Canvas c)
+        {
+            this.setAlive(alive);
+            this.location = loc;
+            this.body.Width = Cell.size;
+            this.body.Height = Cell.size;
+            this.body.Stroke = Brushes.Black;
+            Canvas.SetTop(this.body, location.Y);
+            Canvas.SetLeft(this.body, location.X);
+            this.body.StrokeThickness = 1;
+            this.body.MouseDown += Body_MouseDown;
+            c.Children.Add(this.body);
+        }
+
+        private void Body_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.setAlive(!this.isAlive);
+            Console.WriteLine(this.ToString());
+        }
+
+        public void setAlive(bool living)
+        {
+            this.isAlive = living;
+            this.body.Fill = this.isAlive ? Brushes.Yellow : Brushes.Transparent;
+        }
+        public override string ToString()
+        {
+            return String.Format("a {0} Cell at: {1}, {2}", isAlive ? "Live" : "dead",this.location.X,this.location.Y);
+        }
+    }
+}
---
+++ u5GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿/**
+ * Written by Nolan Meehan.
+ * Simple version of John Conway's Game of Life.
+ * 
+ */using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace u5GameOfLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        Cell[,] board = new Cell[20,20];
+        bool[,] temp = new bool[20, 20];
+public MainWindow()
+        {
+            InitializeComponent();
+            Console.WriteLine("creating board");
+            for(int i = 0; i < board.GetLength(0); i++)
+            {
+                for(int c = 0; c < board.GetLength(1); c++)
+                {
+                    temp[i, c] = false;
+                    board[i, c] = new Cell(false, new Point(c * Cell.size, i * Cell.size), canvas);
+                }
+            }
+            //Console.WriteLine("setting timer");
+            gameTimer.Tick += gameTick;
+            gameTimer.Interval += new TimeSpan(0, 0, 0, 0, 1000 / 10);
+            gameTimer.Start();
+            gameTimer.Stop();
+        }
+
+        private void gameTick(object sender, EventArgs e)
+        {
+            //Console.WriteLine("updating");
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int c = 0; c < board.GetLength(1); c++)
+                {
+                    temp[c, i]= false;
+                }
+            }
+
+            for (int i = 0; i < temp.GetLength(0);i++)
+            {
+                for(int c = 0; c < temp.GetLength(1);c++)
+                {
+                    //Console.WriteLine(board[0, 0].isAlive);//getCellNeighbours(board[0, 0]));
+                    int neighbours = getCellNeighbours(board[i, c]);
+                    if ((neighbours > 3 || neighbours < 2)) temp[i, c]=(false);
+                    else if (!board[i, c].isAlive && neighbours == 2) temp[i, c]=(false);
+                    else temp[i, c]=(true);
+                }
+            }
+            //Console.WriteLine("updated");
+            copyArray(temp);
+        }
+
+        public int getCellNeighbours(Cell cell)
+        {
+            int y = (int)(cell.Location.Y / Cell.size);
+            int x = (int)(cell.Location.X / Cell.size);
+            int count = 0;
+            for (int i =y-1; i<=y+1; i++)
+            {
+                for (int c = x - 1; c <= x+1; c++)
+                {
+                    if (c < 0 || c >= board.GetLength(1) || i < 0 || i >= board.GetLength(0)) continue;
+                    if ((!(c==x&&i==y))&& board[i, c].isAlive) count++;
+
+                }
+            }
+            return count;
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            gameTimer.Stop();
+        }
+
+        private void Resume_Click(object sender, RoutedEventArgs e)
+        {
+            gameTimer.Start();
+        }
+
+
+        private void copyArray(bool[,] source)
+        {
+            for (int i = 0; i < source.GetLength(0); i++)
+            {
+                for (int c = 0; c < source.GetLength(1); c++)
+                {
+                    board[i, c].setAlive(source[i, c]);
+                }
+            }
+        }
+    }
+}
